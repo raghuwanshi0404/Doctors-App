ecf6c04cf8c44ba00c745449d1c93499cb3ccabc
raghuwanshi0404 <jraghuwanshi0404@gmail.com>
2023-08-10 16:13:37 +0530

    made some changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,4 @@
 import React from 'react'
-// import { styled } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import Paper from '@mui/material/Paper';
-// import Grid from '@mui/material/Grid';
 import Banner from '../banner.svg';
 import Bonemarrow from '../bone marrow.svg';
 import anesth from '../anesthesiology.svg';
